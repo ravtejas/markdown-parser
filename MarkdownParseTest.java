e63c9857dbf82ef86e8e3159efeceb3e0004f3ec
ezh247467 <e8huang@ucsd.edu>
2022-04-28 12:16:48 -0700

    Added commands for JUnit Test

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -4,6 +4,10 @@ import java.util.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+/*
+javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
+java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
+*/
 
 public class MarkdownParseTest {
     @Test
