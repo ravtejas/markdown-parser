2ae1ff5dff3ef616a8781a20d604361073dbc3e3
ezh247467 <e8huang@ucsd.edu>
2022-05-12 12:05:15 -0700

    Deleting comments

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -22,12 +22,4 @@ public class MarkdownParseTest {
         ArrayList<String> links = MarkdownParse.getLinks(content);
         assertEquals(List.of("thereallink.com"), links);
     }
-    
-    /*@Test
-    public void testFailFile() throws IOException {
-        Path fileName = Path.of("broken-everything-file.md");
-        String content = Files.readString(fileName);
-        ArrayList<String> links = MarkdownParse.getLinks(content);
-        assertEquals(List.of("website", "([[[[link]]])"), links);
-    }*/
 }
